e9f0adcf798fd39cff9d0b4e2eb1951073eed005
Nathália Neves Afonso <nattnevesafonso@gmail.com>
2023-03-30 14:15:45 -0300

    revisao

---
+++ Fundamentos/Seção.3/dia3.01/Switch e Case.js
@@ -0,0 +1,18 @@
+let resultadoCandidato = "aprovada";
+
+switch (resultadoCandidato) {
+    case 'aprovada':
+    console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+    break;
+
+    case 'lista':
+    console.log("Você está na nossa lista de espera.");
+    break;
+
+    case 'reprovada':
+    console.log('Infelizmente, você reprovou.');
+    break;
+
+    default:
+    console.log('Informação incorreta.');
+}
